561d2b3cc54f2fa68b0ae637289e167d0af9867d
Shubham <shubhamboss010@gmail.com>
2024-03-02 23:03:31 +0530

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx 	new file:   app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx 	new file:   app/(auth)/error.tsx 	new file:   app/(auth)/layout.tsx 	new file:   app/(dashboard)/(routes)/code/constants.ts 	new file:   app/(dashboard)/(routes)/code/page.tsx 	new file:   app/(dashboard)/(routes)/conversation/constants.ts 	new file:   app/(dashboard)/(routes)/conversation/page.tsx 	new file:   app/(dashboard)/(routes)/dashboard/page.tsx 	new file:   app/(dashboard)/(routes)/image/constants.ts 	new file:   app/(dashboard)/(routes)/image/page.tsx 	new file:   app/(dashboard)/(routes)/music/constants.ts 	new file:   app/(dashboard)/(routes)/music/page.tsx 	new file:   app/(dashboard)/(routes)/settings/constants.ts 	new file:   app/(dashboard)/(routes)/settings/page.tsx 	new file:   app/(dashboard)/(routes)/video/constants.ts 	new file:   app/(dashboard)/(routes)/video/page.tsx 	new file:   app/(dashboard)/error.tsx 	new file:   app/(dashboard)/layout.tsx 	new file:   app/(landing)/error.tsx 	new file:   app/(landing)/layout.tsx 	new file:   app/(landing)/page.tsx 	new file:   app/api/code/route.ts 	new file:   app/api/conversation/route.ts 	new file:   app/api/image/route.ts 	new file:   app/api/music/route.ts 	new file:   app/api/stripe/route.ts 	new file:   app/api/video/route.ts 	new file:   app/api/webhook/route.ts 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   components.json 	new file:   components/bot-avatar.tsx 	new file:   components/crisp-chat.tsx 	new file:   components/crisp-provider.tsx 	new file:   components/free-counter.tsx 	new file:   components/heading.tsx 	new file:   components/landing-content.tsx 	new file:   components/landing-hero.tsx 	new file:   components/landing-navbar.tsx 	new file:   components/loader.tsx 	new file:   components/mobile-sidebar.tsx 	new file:   components/modal-provider.tsx 	new file:   components/navbar.tsx 	new file:   components/pro-modal.tsx 	new file:   components/sidebar.tsx 	new file:   components/subscription-button.tsx 	new file:   components/theme-provider.tsx 	new file:   components/toaster-provider.tsx 	new file:   components/tool-card.tsx 	new file:   components/ui/avatar.tsx 	new file:   components/ui/badge.tsx 	new file:   components/ui/button.tsx 	new file:   components/ui/card.tsx 	new file:   components/ui/command.tsx 	new file:   components/ui/dialog.tsx 	new file:   components/ui/dropdown-menu.tsx 	new file:   components/ui/empty.tsx 	new file:   components/ui/form.tsx 	new file:   components/ui/input.tsx 	new file:   components/ui/label.tsx 	new file:   components/ui/progress.tsx 	new file:   components/ui/select.tsx 	new file:   components/ui/separator.tsx 	new file:   components/ui/sheet.tsx 	new file:   components/user-avatar.tsx 	new file:   constants.ts 	new file:   hooks/use-pro-modal.ts 	new file:   lib/api-limit.ts 	new file:   lib/prismadb.ts 	new file:   lib/stripe.ts 	new file:   lib/subscription.ts 	new file:   lib/utils.ts 	new file:   middleware.ts 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   prisma/schema.prisma 	new file:   public/chat.png 	new file:   public/empty.png 	new file:   public/home.png 	new file:   public/logo.png 	new file:   public/mail.png 	new file:   public/next.svg 	new file:   public/photo.png 	new file:   public/pro.png 	new file:   public/transcript.png 	new file:   public/vercel.svg 	new file:   public/voice.png 	new file:   tailwind.config.js 	new file:   tsconfig.json

---
+++ app/api/music/route.ts
@@ -0,0 +1,54 @@
+
+import Replicate from "replicate";
+import OpenAI from "openai";
+
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(
+  req: Request
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt  } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt is required", { status: 400 });
+    }
+
+    const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
+
+    if (!freeTrial && !isPro) {
+      return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+    }
+
+    const mp3 = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: "alloy",
+      input: prompt,
+    });
+
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    const audioBase64 = buffer.toString("base64");
+
+    if (!isPro) {
+      await incrementApiLimit();
+    }
+
+    return new NextResponse(JSON.stringify({ audio: `data:audio/mp3;base64,${audioBase64}` }), { headers: { "Content-Type": "application/json" } });  } catch (error) {
+    console.log('[MUSIC_ERROR]', error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
