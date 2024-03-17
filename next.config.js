d26fda515458ec1fb8b21769202e183253a85c0c
Shubham <shubhamboss010@gmail.com>
2024-03-17 23:25:44 +0530

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -9,15 +9,5 @@ const nextConfig = {
       "assets.aceternity.com",
     ]
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      Object.assign(config.resolve.alias, {
-        'react': 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-    return config
-  },
 }
 module.exports = nextConfig
