41ee37bbb2b1669d8cc07dbb7e1c122f7bef9211
Shubham <shubhamboss010@gmail.com>
2024-03-20 19:12:58 +0530

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "googleusercontent.com",
-      "oaidalleapiprodscus.blob.core.windows.net",
-      "cdn.openai.com",
-      "images.unsplash.com",
-      "assets.aceternity.com",
+    remotePatterns: [
+      { protocol: 'https', hostname: 'googleusercontent.com' },
+      { protocol: 'https', hostname: 'oaidalleapiprodscus.blob.core.windows.net' },
+      { protocol: 'https', hostname: 'cdn.openai.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'assets.aceternity.com' },
     ]
   },
 }
-module.exports = nextConfig
+
+module.exports = nextConfig;
