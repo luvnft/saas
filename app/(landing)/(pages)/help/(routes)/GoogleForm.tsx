89b030cc54a91749921742d4af4c13cfbe237f91
Shubham <shubhamboss010@gmail.com>
2024-03-24 21:42:58 +0530

    modified:   app/(landing)/(pages)/help/(routes)/GoogleForm.tsx 	modified:   app/(landing)/(pages)/help/page.tsx

--- app/(landing)/(pages)/help/(routes)/GoogleForm.tsx
+++ app/(landing)/(pages)/help/(routes)/GoogleForm.tsx
@@ -4,14 +4,12 @@ import React from 'react';
 
 const GoogleForm: React.FC = () => {
   return (
-    <div>
+    <div style={{ width: '100%', height: '100vh' }}>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSe6u82w3Sa9Wm7QjzGiiDbii0WQRP_9iL4BRP4CBFaeEc-fiw/viewform?embedded=true"
-        width="640"
-        height="382"
-        frameBorder="0"
-        marginHeight={0}
-        marginWidth={0}
+        width="100%"
+        height="100%"
+        
       >
         Loading…
       </iframe>
--- app/(landing)/(pages)/help/page.tsx
+++ app/(landing)/(pages)/help/page.tsx
@@ -7,6 +7,11 @@ import { SparklesPreview } from "./(routes)/sparks";
 
 import React from 'react';
 import GoogleForm from './(routes)/GoogleForm';
+import {  Mail, } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from 'next/link';
+
+
 
 const helpPage: React.FC = () => {
   return (
@@ -16,8 +21,27 @@ const helpPage: React.FC = () => {
             <LandingMiddle />   
             <SparklesPreview/>   
             
-<div className="flex flex-row items-center justify-center  w-full md:text-1xl text-1xl lg:text-2xl font-semibold ">
-      <h1></h1>
+            <div>
+            <div style={{ textAlign: 'left', marginLeft: '25%', marginRight: '25%', marginTop: '5vh' }}>
+        <p style={{ textAlign: 'center',  }}>
+          Write us at <a  href="mailto:help@cogify.social?subject=I%20need%20help%20related%20to.">help@cogify.social</a> for better help
+             
+             </p>
+             <div className="flex justify-center" style={{ width: '100%', marginTop: '20px' }}> 
+             <Link href={'mailto:help@cogify.social?subject=I%20need%20help%20related%20to'}>
+
+             <Button className="px-8 py-2 rounded-md bg-zinc-800 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-blue-500">
+          Urgent Help
+          </Button> 
+
+          </Link>
+          </div>
+        <div style={{ textAlign: 'center', marginTop: '10px' }}>
+          
+        </div>
+        <hr style={{ borderColor: 'black', borderWidth: '1px', marginTop: '10px', marginBottom: '10px' }} />
+      </div>
+      
       <GoogleForm />
       </div>
       <Footer/>
