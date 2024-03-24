21c4798f2b48eded35b448a1f4afb084b7614c55
Shubham Sharma <96993718+Shubham-sharma8@users.noreply.github.com>
2024-03-24 22:42:43 +0530

    Update landing-navmiddle.tsx

--- components/landing-navmiddle.tsx
+++ components/landing-navmiddle.tsx
@@ -131,7 +131,7 @@ export function LandingMiddle() {
               Need assistance? We&apos;ar here to help! Fill out the form below and let us guide you through any questions or concerns you have with Cogify.     
               </ListItem>
               <ListItem href="/affiliation" title="Affiliation">
-              "Interested in affiliating with Cogify? Submit your request here and let's explore partnership opportunities together."
+              Interested in affiliating with Cogify? Submit your request here and let&apos;s explore partnership opportunities together!
               </ListItem>
             </ul>
           </NavigationMenuContent>
