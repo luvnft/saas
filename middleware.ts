5649341b33c06a707db6c3c31b1a4590ad8535af
Shubham <shubhamboss010@gmail.com>
2024-03-25 08:33:47 +0530

    modified:   middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ export default authMiddleware({
   publicRoutes: ["/", "/api/webhook", "/about-us", "/privacy-policy", "/term-of-service", "/contact-us", "/careers", "/help", "/feedback", "/refund", "/affiliation"],
 });
 
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
