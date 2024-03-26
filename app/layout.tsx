8b78a5a58a6a994353b359814a69ab9e9dee0cac
Shubham <shubhamboss010@gmail.com>
2024-03-27 01:45:10 +0530

    modified:   app/layout.tsx 	modified:   components/landing-hero.tsx 	new file:   public/highLightTitle.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,73 +32,15 @@ export function generateViewport(): Viewport {
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  category: 'technology',
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
+  
 
-  metadataBase: new URL('https://cogify.social'),
-  alternates: {
-    canonical: '/',
-    languages: {
-      'en-US': '/en-US',
-      'de-DE': '/de-DE',
-    },
-  },
-
-  openGraph: {
   title:  {
     default: 'Cogify',
     template: '%s - Cogify',
   },
   
   description: "Unleash AI Creativity - Prompt, Generate, Speak, Entertain, Code",
-  url: 'https://cogify.studio',
-    siteName: 'Cogify',
-    images: [
-      {
-        url: 'https://cogify.social/logo.png',
-        alt: 'Cogify - Logo', // Must be an absolute URL
-        
-      },
-      {
-        url: 'https://cogify.social/1.svg', // Must be an absolute URL
-        alt: 'an alien planet&apos;s ecosystem, featuring bizarre flora and fauna, strange geology, and atmospheric anomalies',
-      },
-      {
-        url: 'https://cogify.social/2.svg', // Must be an absolute URL
-        alt: 'mandalas incorporating vibrant colors and unique patterns that will inspire feelings of happiness and well-being.',
-      },
-      {
-        url: 'https://cogify.social/3.svg', // Must be an absolute URL
-        alt: 'literary classic 1700&apos;s, blending indian traditional and modern elements to add a unique artistic touch for a marriage.',
-      },
-      {
-        url: 'https://cogify.social/4.svg', // Must be an absolute URL
-        alt: 'an anime character with large, detailed eyes and distinctive Japanese anime aesthetics',
-      },
-      {
-        url: 'https://cogify.social/4.svg', // Must be an absolute URL
-        alt: 'an anime character with large, detailed eyes and distinctive Japanese anime aesthetics',
-      },
-      
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  other: {
-    custom: 'meta',
-  },
+  viewport: generateViewport(),
 }
 
 
--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,7 +12,7 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl text-zinc-900 md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
+        The Best AI Tool for
         <div className=" bg-clip-text text-blue-500 dark:text-blue-500">
           <TypewriterComponent
             options={{
