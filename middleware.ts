806f7f4878a4607be6e378e5207222c0a8d82a26
Shubham <shubhamboss010@gmail.com>
2024-04-01 14:33:32 +0530

    modified:   middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextResponse } from 'next/server';
 
-export function middleware(request: Request) {
-  const response = NextResponse.next();
-
-  // Set the Strict-Transport-Security header
-  response.headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
-
-  return response;
-}
 
 
 
