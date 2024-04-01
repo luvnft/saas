90627e347ef57f533718b8102a2090325a625788
Shubham <shubhamboss010@gmail.com>
2024-04-01 14:27:56 +0530

    modified:   middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from 'next/server';
+
+export function middleware(request: Request) {
+  const response = NextResponse.next();
+
+  // Set the Strict-Transport-Security header
+  response.headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+
+  return response;
+}
+
+
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook", "/about-us", "/privacy-policy", 
@@ -10,3 +22,4 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
