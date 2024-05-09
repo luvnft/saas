58df6fb2c170c13d42ce50e4eee1f65c7df6787b
Shubham <shubhamboss010@gmail.com>
2024-05-09 13:26:54 +0530

    modified:   components/typing-effect.tsx

--- components/typing-effect.tsx
+++ components/typing-effect.tsx
@@ -20,7 +20,6 @@ export function TypewriterEffectSmoothDemo() {
       className: "text-blue-500 dark:text-blue-500",
     },
   ];
-
   return (
     <div className="flex flex-col items-center justify-center h-[20rem]">
       <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base">
