d62103472a20e5bd57fbe28636402f54f650aeb6
Shubham <shubhamboss010@gmail.com>
2024-05-09 13:34:24 +0530

    modified:   components/typing-effect.tsx

--- components/typing-effect.tsx
+++ components/typing-effect.tsx
@@ -2,6 +2,7 @@
 import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
+import Image from "next/image";
 
 export function TypewriterEffectSmoothDemo() {
   const { isSignedIn } = useAuth();
@@ -21,7 +22,7 @@ export function TypewriterEffectSmoothDemo() {
     },
   ];
 
-  
+
   return (
     <div className="flex flex-col items-center justify-center h-[20rem]">
       <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base">
@@ -31,7 +32,7 @@ export function TypewriterEffectSmoothDemo() {
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 items-center">
         <Link href={"https://github.com/Shubham-sharma8/Cogify.Social"}>
         <button className="w-60 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm flex items-center justify-center space-x-2">
-  <img src="/github.png" alt="GitHub Logo" className="h-7 w-7" /> {/* Replace the URL with the actual GitHub logo URL */}
+  <Image src="/github.png" alt="GitHub Logo" className="h-7 w-7" /> {/* Replace the URL with the actual GitHub logo URL */}
   <span className="text-white">Star us on GitHub</span>
 </button>
 
