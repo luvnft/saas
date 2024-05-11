322c2b3cb7ee95e3ecdf82cd4588977381bb82a6
Shubham <shubhamboss010@gmail.com>
2024-05-11 13:19:21 +0530

    modified:   components/typing-effect.tsx

--- components/typing-effect.tsx
+++ components/typing-effect.tsx
@@ -35,7 +35,7 @@ export function TypewriterEffectSmoothDemo() {
   return (
     <div className="flex flex-col items-center justify-center h-[20rem]">
       <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base">
-       We won&apos;t spam ever! We promise.
+       We won&apos;t spam ever! We promise.🙅‍♂️
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 items-center">
