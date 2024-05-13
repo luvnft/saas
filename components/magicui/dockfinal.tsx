ae857cde989d86f79f17e5a0ff557580d3430b0f
Shubham <shubhamboss010@gmail.com>
2024-05-13 17:28:59 +0530

    modified:   components/magicui/dockfinal.tsx 	modified:   components/magicui/retro.tsx

--- components/magicui/dockfinal.tsx
+++ components/magicui/dockfinal.tsx
@@ -1,20 +1,25 @@
 import { Dock, DockIcon } from "./dock";
 import React from "react";
+import Link from "next/link";
+
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
 export function Dockfinal() {
   return (
     <div className="relative flex h-[400px] w-full max-w-[32rem] flex-col items-center justify-center overflow-hidden rounded-lg border md:shadow-xl">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        More? 
-      </span>
+      <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent mb-10">
+           Also Available
+         </span>
       <Dock>
+      
         <DockIcon>
+        <Link href={"https://github.com/Shubham-sharma8/saas"}>
           <Icons.gitHub className="h-6 w-6" />
+          </Link> 
         </DockIcon>
         <DockIcon>
-          <Icons.googleDrive className="h-6 w-6" />
+          <Icons.telegram className="h-6 w-6" />
         </DockIcon>
         <DockIcon>
           <Icons.notion className="h-6 w-6" />
@@ -23,7 +28,10 @@ export function Dockfinal() {
           <Icons.openai className="h-6 w-6" />
         </DockIcon>
         <DockIcon>
+          <Link href={"https://wa.me/919831047163?text=Hi"}>
           <Icons.whatsapp className="h-6 w-6" />
+          </Link>
+          
         </DockIcon>
       </Dock>
       <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
@@ -60,6 +68,11 @@ const Icons = {
         fill="#000"
       />
     </svg>
+  ),
+  telegram: (props: IconProps) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="2 4 42 42"   {...props}>
+<path fill="#29b6f6" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"></path><path fill="#fff" d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"></path><path fill="#b0bec5" d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"></path><path fill="#cfd8dc" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"></path>
+</svg>
   ),
   openai: (props: IconProps) => (
     <svg role="img" viewBox="0 0 24 24" {...props}>
