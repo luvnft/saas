586d4e10fb12ec087241e50f5ffa44f237de0a24
Shubham <shubhamboss010@gmail.com>
2024-05-24 19:51:05 +0530

    Ok

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,6 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
+
 const prismadb = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
